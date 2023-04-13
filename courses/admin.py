ce20574f9787d05dc7c48f53a71fb4d6ef6ff1e3
gustavo <cigu1966@gamil.com>
2023-04-13 15:24:17 -0300

    configs

--- courses/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from courses.models import Course
-
-@admin.register(Course)
-class CourseAdmin(admin.ModelAdmin):
-    list_display = ('name', 'teacher', 'created_at')
